b328656a5eaf8fe6acd501ad45e02882f5e456f5
Menno Buggenhout <mennobuggenhout@gmail.com>
2026-04-21 01:18:32 +0200

    Added alternate mode to imagefader

--- Assets/Scripts/UI/ImageFader.cs
+++ Assets/Scripts/UI/ImageFader.cs
@@ -1,73 +1,123 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ImageFader : MonoBehaviour
 {
-    private int CurrentNumImages => images?.Length ?? 0;
-
     public Image[] images;
-    
+    public Sprite[] alternateImages;
+
+    public bool alternateMode;
+
     public float fadeVal;
 
-    private float lastFadeVal;
+    private Sprite[] _originalSprites;
+    private bool _lastAlternateMode;
+    private float _lastFadeVal;
+
+    private int CurrentNumImages => images?.Length ?? 0;
+
+    private void Awake()
+    {
+        if (images != null)
+            _originalSprites = images.Select(img => img.sprite).ToArray();
+    }
 
     private void Start()
     {
-        if(images.Length < 3)
-            return;
-        
-        if (images[2].sprite == null)
-        {
-            images[2].gameObject.SetActive(false);
-            images = images.Take(2).ToArray();
-        }
+        InitImageArray(ref images);
+        _lastAlternateMode = alternateMode;
     }
 
     private void Update()
     {
-        if (!Mathf.Approximately(lastFadeVal, fadeVal))
+        bool modeChanged = _lastAlternateMode != alternateMode;
+
+        if (modeChanged)
         {
-            lastFadeVal = fadeVal;
-            
+            _lastAlternateMode = alternateMode;
+            SwapSprites(alternateMode);
+        }
+
+        if (modeChanged || !Mathf.Approximately(_lastFadeVal, fadeVal))
+        {
+            _lastFadeVal = fadeVal;
             SetFadeVal(fadeVal);
         }
     }
 
     public void SetFadeVal(float fadeVal)
     {
-        if(CurrentNumImages == 0)
+        if (CurrentNumImages == 0)
         {
-            Debug.LogWarning($"No images, can't fade");
+            Debug.LogWarning("No images in active set, can't fade");
             return;
         }
-        if(CurrentNumImages == 1)
+        if (CurrentNumImages == 1)
         {
-            Debug.LogWarning($"Only 1 image, can't fade");
+            Debug.LogWarning("Only 1 image in active set, can't fade");
             return;
         }
+
         float percentagePerSource = 1f / (float)(CurrentNumImages - 1);
 
         int startSample = Mathf.FloorToInt(fadeVal / percentagePerSource);
         float remainder = fadeVal - (percentagePerSource * startSample);
         float remainderPercentage = remainder / percentagePerSource;
-        
+
         for (int i = 0; i < CurrentNumImages; i++)
         {
             Image image = images[i];
-            
+
             float alpha = 0;
-            
+
             if (i == startSample)
                 alpha = 1 - remainderPercentage;
             if (i == startSample + 1)
                 alpha = remainderPercentage;
-        
-            Color currentColor = image.color;
-            image.color = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+
+            Color c = image.color;
+            image.color = new Color(c.r, c.g, c.b, alpha);
+        }
+    }
+
+    private void SwapSprites(bool useAlternate)
+    {
+        if (_originalSprites == null)
+        {
+            Debug.LogWarning("ImageFader: cache was empty on SwapSprites, caching now.");
+            _originalSprites = images.Select(img => img.sprite).ToArray();
+        }
+
+        Sprite[] source = useAlternate ? alternateImages : _originalSprites;
+
+        if (source == null)
+        {
+            Debug.LogWarning("ImageFader: no sprites to swap to.");
+            return;
+        }
+
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (i >= source.Length)
+            {
+                Debug.LogWarning($"ImageFader: alternateImages is shorter than images (missing index {i}).");
+                break;
+            }
+
+            images[i].sprite = source[i];
+        }
+    }
+
+    private void InitImageArray(ref Image[] imageArray)
+    {
+        if (imageArray == null || imageArray.Length < 3)
+            return;
+
+        if (imageArray[2].sprite == null)
+        {
+            imageArray[2].gameObject.SetActive(false);
+            imageArray = imageArray.Take(2).ToArray();
         }
     }
 }
